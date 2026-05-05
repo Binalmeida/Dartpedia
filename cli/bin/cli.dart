3f24725876db5e1b420c4345096c65d242849c70
Binalmeida <boliveiralmeida83@gmail.com>
2026-05-05 18:05:42 +0000

    V.0.0.2

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,13 +1,9 @@
-const version = '0.0.1';
-
-import 'package:cli/cli.dart' as cli;
-// Importa o pacote cli e usa o apelido "cli" para acessar suas funções
+const version = '0.0.2';
 
 void main(List<String> arguments) {
-// Função principal do programa (ponto de entrada)
+// Função principal
 
-    print('Hello world: ${cli.calculate()}!');
-// Imprime "Hello world" junto com o resultado da função calculate() do pacote cli
+    print('Hello, Dart!');
+// Imprime uma saudação simples no terminal
 
 }
-
