7e437d98d3a91a3332f1049aecdfaacbd3033cf0
guinix10 <limaguilherme140109@gmail.com>
2026-05-19 14:14:21 -0300

    V.0.0.11
    Refactor CLI to use CommandRunner for command management.

--- cli/lib/cli.dart
+++ cli/lib/cli.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
+const String version = '0.0.8';
+/// FUNÇÃO PRINCIPAL
+/// Agora o CommandRunner gerencia todos os comandos.
+/// O main é async para aguardar operações assíncronas.
+Future<void> main(List<String> arguments) async {
+  // Cria o runner
+  final runner = CommandRunner();
+
+  // Executa os comandos recebidos pelo terminal
+  await runner.run(arguments);
 }
