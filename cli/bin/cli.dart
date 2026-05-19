3af5a233b4484e7cd4ed8f65e505b26856a21f3a
Isaac Gonçalves <isaac.silvalima15@gmail.com>
2026-05-19 14:11:14 -0300

    V.0.0.9

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
 const String version = '0.0.8';
 
 Future<void> runCommands(List<String> arguments) async {
@@ -8,7 +11,6 @@ Future<void> runCommands(List<String> arguments) async {
   } else if (arguments.first == 'search') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
 
-   
     await searchWikipedia(inputArgs);
   } else {
     printUsage();
@@ -26,21 +28,17 @@ Future<void> searchWikipedia(List<String>? arguments) async {
     return;
   }
 
-   final query = arguments.join(' ');
+  final query = arguments.join(' ');
 
- 
   print('Resultado da busca para "$query":');
   print('- Página encontrada: $query (simulado)');
 }
-
 
 void printUsage() {
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
 }
-
-
 
 /// Função main que o Dart executa ao rodar a CLI
 Future<void> main(List<String> arguments) async {
